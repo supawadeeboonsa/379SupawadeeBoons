e83d7215cd60a76aa9962732a4e8dcaaec35ef77
supawadeeboonsa <baipo2511@gmail.com>
2025-10-08 11:59:49 +0700

    CI/CD

--- quasar.config.js
+++ quasar.config.js
@@ -36,7 +36,7 @@ export default defineConfig((ctx) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#build
     build: {
-      // publicPath: '/',
+      publicPath: '/379SupawadeeBoons/',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // webpackTranspile: false,
